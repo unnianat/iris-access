371404e1b714f76542cd3e70e674be3b791e02ad
ryanlramos <ramos_ryan@yahoo.com>
2017-06-27 10:55:30 -0700

    modified:   bin/run.js 	new file:   node_modules/dotenv/CHANGELOG.md 	new file:   node_modules/dotenv/LICENSE 	new file:   node_modules/dotenv/README.md 	new file:   node_modules/dotenv/config.js 	new file:   node_modules/dotenv/lib/main.js 	new file:   node_modules/dotenv/package.json 	new file:   package-lock.json 	modified:   package.json 	modified:   server/service.js

--- bin/run.js
+++ bin/run.js
@@ -3,6 +3,8 @@ const request = require('superagent');
 const service = require('../server/service');
 const http = require('http');
 
+require('dotenv').config({path: '../.env'});
+
 const server = http.createServer(service);
 server.listen();
 
@@ -10,7 +12,7 @@ server.on('listening', function() {
     console.log(`IRIS-Access is listening on ${server.address().port} in ${service.get('env')} mode.`);
 
     const announce = () => {
-        request.put(`http://127.0.0.1:3000/service/access/${server.address().port}`, (err, res) => {
+        request.put(`http://` + process.env.SERVER_HOST + `:` + process.env.SERVER_PORT + `/service/access/${server.address().port}`, (err, res) => {
             if(err) {
                 console.log(err);
                 console.log("Error connecting to Iris"); 
--- server/service.js
+++ server/service.js
@@ -4,10 +4,37 @@ const express = require('express');
 const service = express();
 const request = require('superagent');
 
+require('dotenv').config({path: '../.env'});
+
 service.get('/service/:application', (req, res, next) => {
 	if( req.params.application == 'Reach' )    
 	{
 		res.json({result: `Unnikrishnan Kavungal Anat`});
+
+
+var auth = new Buffer( process.env.SERVICE_NOW_USER + ':' + process.env.SERVICE_NOW_PASSWORD).toString('base64'); //create --user user:password to add to header
+
+request.post(process.env.SERVICE_NOW_URL)
+    .set('Content-Type', 'application/json')
+	.set('Accept','application/json')
+	.set('Authorization','Basic ' + auth) //adding authentication
+    .send('{\'short_description\':\'Unable to connect to office wifi again\',' +
+		   '\'assignment_group\':\'287ebd7da9fe198100f92cc8d1d2154e\',' +
+		   '\'urgency\':\'2\',' +
+		   '\'impact\':\'2\'}')
+    .end(function(err, res){
+		if(err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }else{
+			console.log("Success");
+			
+		}
+    });
+
+
+
+
 	}
 	else
 	{
